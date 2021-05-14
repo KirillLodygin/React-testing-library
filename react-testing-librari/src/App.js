4d423ab433e0d8832222b364cb2fcf6ec88a2138
Kirilll <vilyam13@gmail.com>
2021-05-14 20:44:08 +0300

    Search Variants

--- react-testing-librari/src/App.js
+++ react-testing-librari/src/App.js
@@ -1,36 +1,50 @@
-import React, { useState } from "react";
-//import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
 import "./App.css";
+
+const getUser = () => Promise.resolve({ id: 1, name: "Yauhen" });
 
 const Search = ({ value, onChange, children }) => (
     <div>
-      <label htmlFor="search">{children}</label>
-      <input
-          placeholder="search text..."
-          id="search"
-          type="text"
-          value={value}
-          onChange={onChange}
-      />
+        <label htmlFor="search">{children}</label>
+        <input
+            placeholder="search text..."
+            id="search"
+            type="text"
+            value={value}
+            onChange={onChange}
+            // required
+        />
     </div>
 );
 
 const App = () => {
-  const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("");
+    const [user, setUser] = useState(null);
 
-  const handleChange = ({ target }) => {
-    setSearch(target.value);
-  };
+    useEffect(() => {
+        const loadUser = async () => {
+            const user = await getUser();
+            setUser(user);
+        };
 
-  return (
-      <div>
-        <img src="" alt="search image" />
-        <Search value={search} onChange={handleChange}>
-          Search:
-        </Search>
-        <p>Searches for {search ? search : "..."}</p>
-      </div>
-  );
+        loadUser();
+    }, []);
+
+    const handleChange = ({ target }) => {
+        setSearch(target.value);
+    };
+
+    return (
+        <div>
+            {user && <h2>Logged in as {user.name}</h2>}
+            <img className="image" src="" alt="search image" />
+            <Search value={search} onChange={handleChange}>
+                SEARCH:
+            </Search>
+            <p>Searches for {search ? search : "..."}</p>
+        </div>
+    );
 };
 
 export default App;
